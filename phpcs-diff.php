5091698a3a6020bdbedbaeba81bf08496db1295b
david-binda <david.binovec@gmail.com>
2017-03-11 16:43:44 +0100

    Adding plugin file with header and conditional WP CLI command loading

---
+++ phpcs-diff.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: PHPCS Diff
+Plugin URI:  https://github.com/Automattic/phpcs-diff
+Description: Add WP-CLI command for generating PHP CodeSniffer reports for diff retrieved from version systems
+Version:     0.0.1
+Author:      Automattic
+Author URI:  https://automattic.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: phpcs_diff
+Domain Path: /languages
+*/
+
+// Load the command only if we're running WordPress via WP CLI.
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once( './wp-cli-command.php' );
+}
